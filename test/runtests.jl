f819e6f417689e5577b917ecb932342176a8fa03
Hendrik Ranocha <ranocha@users.noreply.github.com>
2021-09-01 15:43:28 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 
 @testset "ParallelCoverallsSetup.jl" begin
     @test_nowarn i_am_fully_tested()
-    @test_nowarn i_am_partially_tested(1)
+    @test_nowarn i_am_partially_tested()
     @test_throws AssertionError i_will_throw()
 end
