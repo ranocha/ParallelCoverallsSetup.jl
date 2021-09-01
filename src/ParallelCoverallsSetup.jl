86ba85557bccbc03bdc5f720ec5ed2d5e7fdadf4
Hendrik Ranocha <ranocha@users.noreply.github.com>
2021-09-01 15:39:56 +0200

    Update ParallelCoverallsSetup.jl

--- src/ParallelCoverallsSetup.jl
+++ src/ParallelCoverallsSetup.jl
@@ -2,7 +2,7 @@ module ParallelCoverallsSetup
 
 export foo, i_will_throw
 
-function foo()
+function i_am_fully_tested()
   local val::Int
 
   if Sys.islinux()
@@ -17,6 +17,25 @@ function foo()
 
   return val
 end
+
+
+function i_am_not_tested(x)
+  if x == 1
+    return "x is one"
+  end
+  
+  return "x is not one"
+end
+
+
+function i_am_partially_tested(x)
+  if x == 1
+    return "x is one"
+  else
+    return "x is not one"
+  end
+end
+
 
 function i_will_throw()
   x = rand(10)
